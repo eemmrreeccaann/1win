b54bbd0013533abd34aa1b887805c96b36e10e2b
eemmrreeccaann <119711869+eemmrreeccaann@users.noreply.github.com>
2026-04-17 17:27:52 +0300

    düzenleme mal yapau zekanın hatasını füzeltiyorum

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-'''
-import React, { useState, useEffect } from ''''react'''';
-import ''''./App.css'''';
+import React, { useState, useEffect } from 'react';
+import './App.css';import
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
